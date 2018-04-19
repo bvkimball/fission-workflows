018adc3d50f38f1cd1ffc2cdea5075a591a7a5d4
erwinvaneyk <erwinvaneyk@gmail.com>
2018-04-19 19:08:15 +0200

    Improved debuggability of workflow builder

--- build/build-env/defaultBuild.sh
+++ build/build-env/defaultBuild.sh
@@ -8,23 +8,27 @@ set -e
 parse() {
     src=$1
     dst=$2
-    echo "Parsing ${src} to ${dst}"
+    echo "Parsing ${src} -> ${dst}"
+    cat ${src}
     if ! wfcli parse ${src} > ${dst} ; then
         echo "Failed to parse ${SRC_PKG}"
         exit 12
     fi
+    echo "Completed parsing ${src} -> ${dst}"
 }
 
 echo  "Building with $(wfcli version --client)..."
 if [[ -f ${SRC_PKG} ]] ; then
     # Package is a single file
+    echo "Package is a single file"
     parse ${SRC_PKG} ${DEPLOY_PKG}
 elif [[ -d ${SRC_PKG} ]] ; then
-    # Package is an archive
+    # Package is a directory
+    echo "Package is a directory"
     mkdir -p ${DEPLOY_PKG}
     for wf in ${SRC_PKG}/*.yaml ; do
         dst=$(basename wf)
-        parse ${wf} > ${DEPLOY_PKG}/dst
+        parse ${wf} ${DEPLOY_PKG}/${dst}
     done
 else
     echo "Invalid file type: '${SRC_PKG}'"
